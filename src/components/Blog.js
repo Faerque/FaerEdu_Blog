2129ca754c999ba4106905cc948de132b99064dd
Faerque <undefeatableomar@gmail.com>
2022-09-18 14:09:07 +0600

    Card bug fixes

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -11,59 +11,38 @@ export default function Blog({ blog }) {
     duration,
     category,
   } = blog;
+
   return (
-    <section className="relative bg-gray-50 pt-8 pb-20 px-4 sm:px-6 lg:pt-16 lg:pb-16 lg:px-8">
-      <div className="absolute inset-0">
-        <div className="bg-white h-1/3 sm:h-2/3"></div>
-      </div>
-      <div className="relative max-w-7xl mx-auto">
-        <div className="text-center">
-          <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
-            ALL BLOGS ARE HERE
-          </h2>
-          <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa
-            libero labore natus atque, ducimus sed.
-          </p>
+    <section>
+      {/* <!-- single card  --> */}
+      <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+        <div className="flex-shrink-0">
+          <img className="h-48 w-full object-cover" src={thumbImg} alt="" />
         </div>
 
-        {/* <!-- card grid  --> */}
-        <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
-          {/* <!-- single card  --> */}
-          <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+        <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+          <div className="flex-1">
+            <p className="text-sm font-medium text-indigo-600">
+              <span className="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-indigo-100 text-indigo-800">
+                {category}
+              </span>
+            </p>
+            <a href="#" className="block mt-1">
+              <p className="text-xl font-semibold text-gray-900">{name}</p>
+            </a>
+          </div>
+          <div className="mt-6 flex items-center">
             <div className="flex-shrink-0">
-              <img className="h-48 w-full object-cover" src={thumbImg} alt="" />
+              <img className="h-10 w-10 rounded-full" src={authorImg} alt="" />
             </div>
-
-            <div className="flex-1 bg-white p-6 flex flex-col justify-between">
-              <div className="flex-1">
-                <p className="text-sm font-medium text-indigo-600">
-                  <span className="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-indigo-100 text-indigo-800">
-                    {category}
-                  </span>
-                </p>
-                <a href="#" className="block mt-1">
-                  <p className="text-xl font-semibold text-gray-900">{name}</p>
-                </a>
-              </div>
-              <div className="mt-6 flex items-center">
-                <div className="flex-shrink-0">
-                  <img
-                    className="h-10 w-10 rounded-full"
-                    src={authorImg}
-                    alt=""
-                  />
-                </div>
-                <div className="ml-3">
-                  <p className="text-sm font-medium text-gray-900 hover:underline">
-                    {authorName}
-                  </p>
-                  <div className="flex space-x-1 text-sm text-gray-500">
-                    <time dateTime="2020-03-16">{date}</time>
-                    <span aria-hidden="true">&middot;</span>
-                    <span> {duration} </span>
-                  </div>
-                </div>
+            <div className="ml-3">
+              <p className="text-sm font-medium text-gray-900 hover:underline">
+                {authorName}
+              </p>
+              <div className="flex space-x-1 text-sm text-gray-500">
+                <time dateTime="2020-03-16">{date}</time>
+                <span aria-hidden="true">&middot;</span>
+                <span> {duration} </span>
               </div>
             </div>
           </div>
--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -5,10 +5,27 @@ export default function BlogList() {
   const blogs = useSelector((state) => state.blogData);
 
   return (
-    <>
-      {blogs.map((blog) => (
-        <Blog blog={blog} key={blog.id}></Blog>
-      ))}
-    </>
+    <section className="relative bg-gray-50 pt-8 pb-20 px-4 sm:px-6 lg:pt-16 lg:pb-16 lg:px-8">
+      <div className="absolute inset-0">
+        <div className="bg-white h-1/3 sm:h-2/3"></div>
+      </div>
+      <div className="relative max-w-7xl mx-auto">
+        <div className="text-center">
+          <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
+            ALL BLOGS ARE HERE
+          </h2>
+          <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa
+            libero labore natus atque, ducimus sed.
+          </p>
+        </div>
+        {/* Card Gird */}
+        <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+          {blogs.map((blog) => (
+            <Blog key={blog.id} blog={blog} />
+          ))}
+        </div>
+      </div>
+    </section>
   );
 }
